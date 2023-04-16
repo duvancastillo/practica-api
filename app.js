1bad1bd8df76d6289ce1d5b270d5f6831685dadd
duvancastillo <duvancastillohurtado@gmail.com>
2023-04-15 19:40:19 -0500

    lista de todo

--- app.js
+++ app.js
@@ -1,134 +1,56 @@
 const formulario = document.getElementById("formulario");
-const cardsEstudiantes = document.getElementById("cardsEstudiantes");
-const cardsProfesores = document.getElementById("cardsProfesores");
-const templateEstudiantes = document.getElementById(
-  "templateEstudiantes"
-).content;
-const templateProfesores =
-  document.getElementById("templateProfesores").content;
+const pintarTodo = document.getElementById("pintarTodo");
+const templateTodo = document.getElementById("templateTodo").content;
 const alert = document.querySelector(".alert");
 
-const estudiantes = [];
-const profesores = [];
-//delegacion de eventos para los botones de aproba y reprobar
+let todos = [];
+
+const agregarTodo = (todo) => {
+  const objetoTodo = {
+    nombre: todo,
+    uid: `${Date.now()}`,
+  };
+  todos.push(objetoTodo);
+  pintarTodos();
+};
+
+const pintarTodos = () => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+  pintarTodo.textContent = "";
+  const fragmet = document.createDocumentFragment();
+  todos.forEach((item) => {
+    const clone = templateTodo.cloneNode(true);
+    clone.querySelector(".lead").textContent = item.nombre;
+    clone.querySelector(".btn").dataset.id = item.uid;
+
+    fragmet.appendChild(clone);
+  });
+
+  pintarTodo.appendChild(fragmet);
+};
+
 document.addEventListener("click", (e) => {
-  if (e.target.dataset.id) {
-    if (e.target.matches(".btn-success")) {
-      estudiantes.map((item) => {
-        if (item.id === e.target.dataset.id) {
-          item.setEstado = true;
-        }
-        return item;
-      });
-    }
-    if (e.target.matches(".btn-danger")) {
-      estudiantes.map((item) => {
-        if (item.id === e.target.dataset.id) {
-          item.setEstado = false;
-        }
-        return item;
-      });
-    }
-    Persona.pintarPersona(estudiantes, "Estudiante");
+  if (e.target.matches(".btn-danger")) {
+    todos = todos.filter((item) => item.uid !== e.target.dataset.id);
+    pintarTodos();
   }
 });
-// clase general para luego heredar a estudiante  profesor
-class Persona {
-  constructor(nombre, edad, id) {
-    this.nombre = nombre;
-    this.edad = edad;
-    this.id = id;
-  }
-  //metodp estatico para pintar tanto profesor como estudiate
-  static pintarPersona(personas, tipo) {
-    if (tipo === "Estudiante") {
-      cardsEstudiantes.textContent = "";
-      const fragmet = document.createDocumentFragment();
-      personas.forEach((item) => {
-        fragmet.appendChild(item.agregarEstudiante());
-      });
-      cardsEstudiantes.appendChild(fragmet);
-    }
-    if (tipo === "Profesor") {
-      cardsProfesores.textContent = "";
-      const fragmet = document.createDocumentFragment();
-      personas.forEach((item) => {
-        fragmet.appendChild(item.agregarProfesor());
-      });
-      cardsProfesores.appendChild(fragmet);
-    }
-  }
-}
-//clase de estiante herdando las propiedades de Persona
-class Estudiante extends Persona {
-  #estado = false;
-  #estudiante = "estudiante";
-  set setEstado(estado) {
-    this.#estado = estado;
-  }
 
-  get getEstudiante() {
-    return this.#estudiante;
-  }
-  //agregadndo nuevo estudiante al templateEstudiante
-  agregarEstudiante() {
-    const clone = templateEstudiantes.cloneNode(true);
-    clone.querySelector(".text-primary").textContent = this.nombre;
-    clone.querySelector("h6").textContent = this.getEstudiante;
-    clone.querySelector(".lead").textContent = this.edad;
-    if (this.#estado) {
-      clone.querySelector(".badge").className = "badge bg-success";
-      clone.querySelector(".btn-success").disabled = true;
-      clone.querySelector(".btn-danger").disabled = false;
-    } else {
-      clone.querySelector(".badge").className = "badge bg-danger";
-      clone.querySelector(".btn-danger").disabled = true;
-      clone.querySelector(".btn-success").disabled = false;
-    }
-    clone.querySelector(".badge").textContent = this.#estado
-      ? "Aprobado"
-      : "Reprobado";
-    clone.querySelector(".btn-success").dataset.id = this.id;
-    clone.querySelector(".btn-danger").dataset.id = this.id;
-    return clone;
-  }
-}
-//clase Profesor herando las propiedades de la clase general Persona
-class Profesor extends Persona {
-  #profesor = "profesor";
-  //agregando profesoras al templateProfesores
-  agregarProfesor() {
-    const clone = templateProfesores.cloneNode(true);
-    clone.querySelector("h5").textContent = this.nombre;
-    clone.querySelector("h6").textContent = this.#profesor;
-    clone.querySelector(".lead").textContent = this.edad;
-    return clone;
-  }
-}
-generarIdUnico1 = () => Math.random().toString(30).substring(2);
-
-// se captura los datos desde el formulario para instanciar porfesores y alumnos
 formulario.addEventListener("submit", (e) => {
   e.preventDefault();
-  alert.classList.add("d-none")
-
-  const datos = new FormData(formulario);
-
-  const [nombre, edad, opcion] = [...datos.values()];
-  //validar que no envien campos vaciso
-  if (!nombre.trim() || !edad.trim()) {
+  alert.classList.add("d-none");
+  const data = new FormData(formulario);
+  const [todo] = [...data.values()];
+  if (!todo.trim()) {
     alert.classList.remove("d-none");
     return;
   }
-  const id = generarIdUnico1();
-  if (opcion === "Estudiante") {
-    const estudiante = new Estudiante(nombre, edad, id);
-    estudiantes.push(estudiante);
-    Persona.pintarPersona(estudiantes, opcion);
-  }
-  if (opcion === "Profesor") {
-    const profesor = new Profesor(nombre, edad, id);
-    profesores.push(profesor);
-    Persona.pintarPersona(profesores, opcion);
+  agregarTodo(todo);
+});
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  if (localStorage.getItem("todos")) {
+    todos = JSON.parse(localStorage.getItem("todos"));
+    pintarTodos();
   }
 });
